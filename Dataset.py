94379f0e8d608ecf3ff69a42b3e273aa3ae8fdaf
relar-ritik <agrawalritik27@gmail.com>
2023-11-27 11:54:37 -0500

    Dataset loader

---
+++ Dataset.py
@@ -0,0 +1,84 @@
+import os
+import io
+import torch
+from torch.utils.data import Dataset
+import h5py
+import numpy as np
+from PIL import Image
+
+
+class T2IGANDataset(Dataset):
+    def __init__(self, dataset_file, split="train"):
+        self.dataset_file = dataset_file
+        self.split = split
+        self.dataset = h5py.File(self.dataset_file, mode='r')[self.split]
+        self.img_names, self.dataset_len = self.get_img_names()
+
+    def get_img_names(self):
+        img_names = [str(k) for k in self.dataset.keys()]
+        length = len(self.dataset)
+        return img_names, length
+
+    def __len__(self):
+        return self.dataset_len
+
+    def __getitem__(self, item):
+        example_name = self.img_names[item]
+        example = self.dataset[example_name]
+
+        right_image = bytes(np.array(example['img']))
+        right_embed = np.array(example['embeddings'], dtype=float)
+
+        wrong_image = bytes(np.array(self.__find_wrong_image(example['class'])))
+        inter_embed = np.array(self.__find_inter_embed(), dtype=float)
+
+        right_image = self.__validate_image(Image.open(io.BytesIO(right_image)).resize((64, 64)))
+        wrong_image = self.__validate_image(Image.open(io.BytesIO(wrong_image)).resize((64, 64)))
+        text = np.array(example['txt']).astype(str)
+
+        return {
+            'right_images': torch.FloatTensor(right_image),
+            'right_embed': torch.FloatTensor(right_embed),
+            'wrong_images': torch.FloatTensor(wrong_image),
+            'inter_embed': torch.FloatTensor(inter_embed),
+            'txt': str(text)
+        }
+
+    def __find_wrong_image(self, category):
+        _c = category
+        while _c == category:
+            item = np.random.randint(len(self.img_names))
+            example_name = self.img_names[item]
+            example = self.dataset[example_name]
+            _c = example['class']
+        return example['img']
+
+    def __find_inter_embed(self):
+        idx = np.random.randint(len(self.img_names))
+        example_name = self.img_names[idx]
+        example = self.dataset[example_name]
+        return example['embeddings']
+
+    def __validate_image(self, img):
+        img = np.array(img, dtype=float)
+        if np.max(img) > 1:
+            img = img / 255
+        if len(img.shape) < 3:
+            rgb = np.empty((64, 64, 3), dtype=np.float32)
+            rgb[:, :, 0] = img
+            rgb[:, :, 1] = img
+            rgb[:, :, 2] = img
+            img = rgb
+
+        return img.transpose(2, 0, 1)
+
+
+if __name__ == '__main__':
+    from torch.utils.data import DataLoader
+
+    dataset = T2IGANDataset(dataset_file=".data/flowers.hdf5", split="train")
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+    for a in dataloader:
+        print(a)
+        break
+
